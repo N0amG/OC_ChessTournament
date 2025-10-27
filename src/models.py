4fd75f25c4841d129c7abe3f8817d498699f2a20
Noam <noamguez0@gmail.com>
2025-10-27 18:27:43 +0100

    Add : MVC architecture

---
+++ src/models.py
@@ -0,0 +1,107 @@
+from dataclasses import dataclass
+from storage import load_json, save_json
+
+
+# ---- Player Model ---- #
+
+
+@dataclass
+class Player:
+    """
+    This class define a player \n
+    :param id: str - The player's unique identifier
+    :param lastname: str - The player's last name
+    :param firstname: str - The player's first name
+    :param birthday: str - The player's birthday in YYYY-MM-DD format
+    """
+
+    id: str
+    lastname: str
+    firstname: str
+    birthday: str
+
+    def register_player(self):
+        player = {
+            "id": self.id,
+            "lastname": self.lastname,
+            "firstname": self.firstname,
+            "birthday": self.birthday,
+        }
+
+        data = load_json("data/players.json", default={})
+
+        data[player["id"]] = player
+
+        save_json("data/players.json", data)
+
+
+# ---- Match Model ---- #
+
+
+@dataclass
+class Match:
+    """
+    This class define a match between two players \n
+    :param player1: Player - The first player
+    :param player2: Player - The second player
+    :param score1: float - The score of the first player
+    :param score2: float - The score of the second player
+    """
+
+    player1: Player
+    player2: Player
+    score1: float = 0
+    score2: float = 0
+
+
+# ---- Round Model ---- #
+
+
+@dataclass
+class Round:
+    """
+    This class define a round in a tournament \n
+    :param name: str - The name of the round
+    :param matches: list[Match] - The list of matches in the round
+    :param started_at: str - The start time of the round
+    :param ended_at: str - The end time of the round
+    """
+
+    name: str
+    matches: list[Match]
+
+    # Both optionnal
+    started_at: str = None
+    ended_at: str = None
+
+
+# ---- Tournament Model ---- #
+
+
+@dataclass
+class Tournament:
+    """
+    This class define a tournament \n
+    :param name: str - The name of the tournament
+    :param location: str - The location of the tournament
+    :param start_date: str - The start date of the tournament in YYYY-MM-DD
+    format
+    :param end_date: str - The end date of the tournament in YYYY-MM-DD format
+    """
+
+    name: str
+    location: str
+    start_date: str  # format YYYY-MM-DD
+    end_date: str  # format YYYY-MM-DD
+    players: list[Player]
+    rounds: list[Round]
+    rounds_count: int = 4
+    current_round: int = 1
+    description: str = ""
+
+
+if __name__ == "__main__":
+    player = Player(
+        id="AB1234", lastname="Doe", firstname="John", birthday="1990-01-01"
+    )
+    player.register_player()
